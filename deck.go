cfab9118c3fa1aa5227512946bac563eadb74106
Gabriel Dzul <gabrieldzul@gmail.com>
2023-09-19 17:27:31 -0600

    Added function to generate a deck from a file

--- deck.go
+++ deck.go
@@ -47,3 +47,15 @@ func (d deck) toString() string {
 func (d deck) saveToFile(filename string) error {
 	return os.WriteFile(filename, []byte(d.toString()), 0666)
 }
+
+func newDeckFromFile(filename string) deck {
+	bs, err := os.ReadFile(filename)
+
+	if err != nil {
+		fmt.Println("Error: ", err)
+		os.Exit(1)
+	}
+
+	full_text := string(bs)
+	return deck(strings.Split(full_text, ","))
+}
