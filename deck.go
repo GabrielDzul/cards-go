440afef21232588cca0f92ab9f56018531049e8d
Gabriel Dzul <gabrieldzul@gmail.com>
2023-09-19 16:59:02 -0600

    Added function to save to file

---
+++ deck.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+// Create a new type of 'deck'
+// which is a slice of strings
+type deck []string
+
+func newDeck() deck {
+	cards := deck{}
+	cardSuits := []string{"Spades", "Hearts", "Diamonds", "Clubs"}
+	cardValues := []string{"ace", "one", "two", "three", "four"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			deckCard := fmt.Sprintf("%s of %s", value, suit)
+			cards = append(cards, deckCard)
+		}
+	}
+	return cards
+}
+
+func (d deck) print() {
+	for i, card := range d {
+		fmt.Println(i, card)
+	}
+}
+
+// Splits the slice in order to return and deck(slice) with the values contained in the handsize
+// and another deck with the remaining cards
+func deal(d deck, handSize int) (deck, deck) {
+	return d[:handSize], d[handSize:]
+}
+
+func (d deck) selfDeal(handSize int) (deck, deck) {
+	return d[:handSize], d[handSize:]
+}
+
+func (d deck) toString() string {
+	return strings.Join(d, ",")
+}
+
+func (d deck) saveToFile(filename string) error {
+	return os.WriteFile(filename, []byte(d.toString()), 0666)
+}
